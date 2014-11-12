cc08110abb5b95c8af993e44385e39778e8967bd
Andrew Nefedkin <andrew.nefedkin@actis.ru>
2014-11-12 19:43:15 +0300

    Actualized build scripts.

--- build.cmd
+++ build.cmd
@@ -2,17 +2,30 @@
 
 setlocal enableextensions
 set PATH=%~dp0\tools;%PATH%
-set BuildTargets=%~dp0\packages\Digillect.Build.Tools.1.2.6\tools\Build.targets
 set EnableNuGetPackageRestore=true
 
-if not exist "%BuildTargets%" (
-	nuget.exe install -o "%~dp0\packages" "%~dp0\.nuget\packages.config"
+pushd %~dp0
+
+if exist .nuget\nuget.exe (
+	set NuGetExe=.nuget\nuget.exe
+) else (
+	set NuGetExe=nuget.exe
+)
+
+if exist .nuget\packages.config (
+	%NuGetExe% install .nuget\packages.config -OutputDirectory packages -Verbosity quiet -NonInteractive
+)
+
+if exist *.sln (
+	for %%f in (*.sln) do %NuGetExe% restore "%%f" -Verbosity quiet -NonInteractive
 )
 
 if not errorlevel 1 (
 	if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-		"%ProgramFiles(x86)%\MSBuild\12.0\Bin\amd64\MSBuild.exe" build.proj %*
+		"%ProgramFiles(x86)%\MSBuild\12.0\Bin\amd64\MSBuild.exe" build.proj /v:m %*
 	) else (
-		"%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe" build.proj %*
+		"%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe" build.proj /v:m %*
 	)
 )
+
+popd
